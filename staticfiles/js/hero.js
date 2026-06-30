d12fd89374c9c70ba5c31cb8be680dd041cec922
harshkabir007 <harshkabir007@gmail.com>
2026-07-01 03:38:16 +0530

    a

---
+++ staticfiles/js/hero.js
@@ -0,0 +1,23 @@
+new Typed("#typing", {
+
+    strings: [
+
+        "Artificial Intelligence",
+
+        "Computer Vision",
+
+        "Robotics",
+
+        "Automation",
+
+        "Machine Learning"
+
+    ],
+
+    typeSpeed: 65,
+
+    backSpeed: 40,
+
+    loop: true
+
+});
---
+++ staticfiles/js/main.js
@@ -0,0 +1,17 @@
+new Typed("#typing", {
+
+    strings: [
+        "AI Engineer",
+        "Computer Vision Engineer",
+        "Robotics Engineer",
+        "ML Engineer",
+        "Automation Engineer"
+    ],
+
+    typeSpeed: 70,
+
+    backSpeed: 40,
+
+    loop: true
+
+});
